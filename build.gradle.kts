cf73628c80ce02f2e4a1de044483237f7ea68095
thomaskraaijeveld <thomaskraaijeveld@hotmail.com>
2024-08-01 12:49:01 +0200

    Add version-catalog-update to sort and update libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.gradle.versions)
+    alias(libs.plugins.version.catalog.update)
 }
