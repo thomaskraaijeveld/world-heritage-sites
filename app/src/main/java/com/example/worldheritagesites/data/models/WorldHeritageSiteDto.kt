9a7227d8ba3e2c95cbda40f3c24a2ba9bd416e24
thomaskraaijeveld <thomaskraaijeveld@hotmail.com>
2024-08-01 13:01:22 +0200

    Add WorldHeritageSite data and domain models.

---
+++ app/src/main/java/com/example/worldheritagesites/data/models/WorldHeritageSiteDto.kt
@@ -0,0 +1,19 @@
+package com.example.worldheritagesites.data.models
+
+data class WorldHeritageSiteDto(
+    val id: String,
+    val year: Int,
+    val target: String,
+    val name: String,
+    val type: String,
+    val region: String,
+    val regionLong: String,
+    val coordinates: String? = null,
+    val lat: Double,
+    val lng: Double,
+    val page: String,
+    val image: String,
+    val imageAuthor: String,
+    val shortInfo: String,
+    val longInfo: String? = null
+)
---
+++ app/src/main/java/com/example/worldheritagesites/domain/models/WorldHeritageSite.kt
@@ -0,0 +1,19 @@
+package com.example.worldheritagesites.domain.models
+
+data class WorldHeritageSite(
+    val id: String,
+    val year: Int,
+    val target: String,
+    val name: String,
+    val type: String,
+    val region: String,
+    val regionLong: String,
+    val coordinates: String?,
+    val lat: Double,
+    val lng: Double,
+    val page: String,
+    val image: String,
+    val imageAuthor: String,
+    val shortInfo: String,
+    val longInfo: String?
+)
